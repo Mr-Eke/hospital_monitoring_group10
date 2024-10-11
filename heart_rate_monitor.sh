761b81222737486e2a9618ec3ddaeeb0b54e4ac7
H-Levison <h.levison@alustudent.com>
2024-10-11 09:40:19 -0700

    Modified the codes and added proper comments for each process

--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -6,19 +6,26 @@
 echo "Enter the Recording Device Name:"
 read device_name
 
+# Variable for holding heart_rate_log.txt
+heart_log="heart_rate_log.txt"
+
 # Display logging message and PID of the script
 echo "Heart rate logging starting..."
 echo "The process ID (PID) is : $$"
-echo "Logging heart rate data to heart_rate_log.txt."
+echo "Logging heart rate data to $heart_log"
 
 # Loop for logging heart rate data every second
 while true; do
+	# Get the current timestamp
 	timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-	heart_rate=$((RANDOM%60 + 100))
+	# Generate random heart rate between 60 and 100
+	heart_rate=$((RANDOM%41 + 60))
 
+	# Log data to the heart_rate_log.txt file
+	echo "$timestamp | $device_name | $heart_rate BPM" >> $heart_log
+
+	# Sleep for 1 second before logging the next data
 	sleep 1
-
-	echo "$timestamp | $device_name | $heart_rate BPM" >> heart_rate_log.txt
 
 done &
