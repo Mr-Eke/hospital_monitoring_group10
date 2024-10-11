e70cd01b589050980aeab384f07f8222cf118be2
H-Levison <h.levison@alustudent.com>
2024-10-11 08:33:17 -0700

    Wrote the commands to prompt for device name and display process ID

--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-# This script records heart rate data every second and stores record to heart_rate_log.txt file
-# It also displays the Process ID (PID) of the recording process
+# This script records heart rate data every second and logs record to heart_rate_log.txt file
+# It also displays the Process ID (PID) of the script
+
+# Request for device name
+echo "Enter the Recording Device Name:"
+read device_name
+
+# Display logging message and PID of the script
+echo "Heart rate logging starting..."
+echo "The process ID (PID) is : $$"
+echo "Logging heart rate data to heart_rate_log.txt."
+
+
