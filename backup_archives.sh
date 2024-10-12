95a3b426653da42f357757b30e1d6288da749436
Mr-Eke <c.eke@alustudent.com>
2024-10-12 16:42:27 +0200

    Remote server backup implemented: Copies all archived files to a remote server

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-# moves and backup archived log files to a remote server
+# Script that moves and backup archived log files to a remote server
 
+userName="2689fa338548"
+hostInfo="2689fa338548.bc49e1b9.alu-cod.online"
 archiveDirectory="archived_logs_group10"
 
-# Checks if directory doesn't exists and creates it
-if [ ! -d "$archiveDirectory" ]; then 
-	mkdir "$archiveDirectory"
-	echo "Directory has been successfully created"
-
-else 
-	echo "The Directory does exist"
-
+# Checks if directory doesn't exists and create it
+if [ ! -d "$archiveDirectory" ]; then
+    mkdir "$archiveDirectory"
+    echo "Directory is successfully created. Moving files now.."
+else
+    echo "Directory already exist. Moving files now.."
 fi
 
-for eachFile in heart_rate_log.txt_*; do 
-	if [ -f "$eachFile" ]; then
-		mv "$eachFile" "$archiveDirectory"
-		echo "The file has been successfully moved"
-	else 
-		echo "File not found"
-
-  	fi 
+# Loop through archived files and move them to the directory
+for eachFile in heart_rate_log.txt_*; do
+    if [ ! -f "$eachFile" ]; then
+        echo "No available files to move"
+    else
+        mv "$eachFile" "$archiveDirectory"
+    fi
 done
+
+# Copy/backup archived data to a remote server
+scp $archiveDirectory/* "$userName@$hostInfo:/home/"
