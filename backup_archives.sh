eb55d4689aa32dc9c5cf997f46fe71802abbc7de
Az-oga <a.oga@alustudent.com>
2024-10-12 05:15:02 -0700

    Properly formatted script and added 'fi' to the end of the if statement

--- backup_archives.sh
+++ backup_archives.sh
@@ -6,7 +6,10 @@ archiveDirectory="archived_logs_group10"
 
 if [ ! -d "$archiveDirectory" ]; then 
 	mkdir "$archiveDirectory"
-echo "Directory has been successfully created"
+	echo "Directory has been successfully created"
 
 else 
-	echo "The Directory does exist" 
+	echo "The Directory does exist"
+
+fi
+
