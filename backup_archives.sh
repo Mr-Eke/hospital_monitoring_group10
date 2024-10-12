f3319f70729abf8fba6745017432eb742ff252b2
Az-oga <a.oga@alustudent.com>
2024-10-12 05:12:05 -0700

    Creates the backup directory if it doesn't exist

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 # moves and backup archived log files to a remote server
+
+archiveDirectory="archived_logs_group10"
+
+if [ ! -d "$archiveDirectory" ]; then 
+	mkdir "$archiveDirectory"
+echo "Directory has been successfully created"
+
+else 
+	echo "The Directory does exist" 
