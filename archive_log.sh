ed25f355f4696872af53ca99adbd135fb4fbc1c0
Burabyo <j.burabyo@alustudent.com>
2024-10-13 02:12:26 -0700

    created archive script and checked if the log file exist

---
+++ archive_log.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Log file to be archived
+LOG_FILE="heart_rate_log.txt"
+
+# Check if the log file exists
+if [ ! -f "$LOG_FILE" ]; then
+	echo "Error:log file $LOG_FILE not found"
+       exit 1
+fi       
